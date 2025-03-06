ca3f905b546f31684cedb4a740f13790303f30d5
陳德生 <111033412+KoukeNeko@users.noreply.github.com>
2025-03-06 16:36:23 +0800

    Update reload command alias for announcement system

--- src/main/java/dev/doeshing/koukeNekoAnnoundement/KoukeNekoAnnoundement.java
+++ src/main/java/dev/doeshing/koukeNekoAnnoundement/KoukeNekoAnnoundement.java
@@ -20,7 +20,7 @@ public final class KoukeNekoAnnoundement extends JavaPlugin {
 
         // 使用 CommandSystem 註冊 reload 指令
         commandSystem = new CommandSystem(this);
-        commandSystem.registerCommand("koukenekoannouncementreload", new ReloadCommand(this), null, "重新載入設定文件", "/knar", "rl");
+        commandSystem.registerCommand("koukenekoannouncementreload", new ReloadCommand(this), null, "重新載入設定文件", "/koukenekoannouncementreload", "knar");
 
         // 啟動公告任務
         scheduleAnnouncements();
