690a7622cbb94b7b559744950dda50dc8a506687
陳德生 <111033412+KoukeNeko@users.noreply.github.com>
2025-03-06 16:18:29 +0800

    Initial commit

---
+++ src/main/java/dev/doeshing/koukeNekoAnnoundement/KoukeNekoAnnoundement.java
@@ -0,0 +1,17 @@
+package dev.doeshing.koukeNekoAnnoundement;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KoukeNekoAnnoundement extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/dev/doeshing/koukeNekoAnnoundement/commands/ReloadCommand.java
@@ -0,0 +1,4 @@
+package dev.doeshing.koukeNekoAnnoundement.commands;
+
+public class ReloadCommand {
+}
